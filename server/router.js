9d46bf6abf5b2479cd090fd584964f15c98d07ea
luyb <luyb@xunzhaotech.com>
2020-05-20 09:43:03 +0800

    feat -添加后端目录结构

---
+++ server/router.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-20 09:37:41
+ * @LastEditTime: 2020-05-20 09:37:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Nuxt-Koa-Admin\server\router.js
+ */ 
